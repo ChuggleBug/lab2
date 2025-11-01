b40c115450cf6afac50138f641616d205fd97aef
Jacob <jacobgutierrez752@gmail.com>
2025-10-31 19:37:18 -0700

    Submission Commit

--- include/app_config.h
+++ include/app_config.h
@@ -3,14 +3,32 @@
 
 #define BAUD_RATE               (PIO_BAUD_RATE)
 
+// Traffic Light pin mappings
 #define TL_RED_GPIO_NUM        (2)
 #define TL_YELLOW_GPIO_NUM     (18)
 #define TL_GREEN_GPIO_NUM      (4)
-
 #define BUTTON_GPIO_NUM        (32)
-
 #define BUZZER_GPIO_NUM        (12)
+
+// Buzzer specific configurations 
 #define BUZZER_FREQ            (440)
+#define BUZZER_CHANNEL   0   // channel 0–15
+#define BUZZER_RESOLUTION 8   // bits (1–16)
+
+
+// ***** Traffic light delays *****
+
+// Red state
+#define TL_DELAY_RED_MS         (10000)
+#define TL_DELAY_RED_BUZZER_MS  (250)
+
+// Yellow and Yellow_Red State
+#define TL_DELAY_YELLOW_MS      (2000)
+
+// Green State
+#define TL_DELAY_GREEN_MS       (5000)
+#define TL_DELAY_RED_BUZZER_ON_MS   (500)
+#define TL_DELAY_RED_BUZZER_OFF_MS  (1500)
 
 typedef enum _TL_STATE {
     TL_STATE_RED,
--- src/main.cpp
+++ src/main.cpp
@@ -18,41 +18,39 @@ void loop() {
   uint16_t tl_delay_epoch;
   // Starting reference point for the buzzer countdown
   uint16_t buzzer_delay_epoch;
-
   // Countdown timer before toggling the buzzer
   uint16_t buzzer_countdown_ms;
-
   // Comparison value for the buzzer countdown
   // Overwrties the reference point (epoch) when count down goes to 0
   uint16_t current_time;
-
   // Is the buzzer on?
   bool buzzer_on = false;
 
   switch (tl_state) {
   case TL_STATE_RED: {
-    current_time = millis();
+    digitalWrite(TL_RED_GPIO_NUM, HIGH);
     
     // Both the tl delay and the buzzer can share the same epoch on start
+    current_time = millis();
     tl_delay_epoch = current_time;
     buzzer_delay_epoch = current_time;
 
-    buzzer_countdown_ms = 250;
-
-    digitalWrite(TL_RED_GPIO_NUM, HIGH);
+    // Countdown for buzzer toggle
+    // This state does not modify this value during the countdown
+    buzzer_countdown_ms = TL_DELAY_RED_BUZZER_MS;
 
     // Until the 10 seconds have passed...
-    while ( ( ( current_time = millis() ) - tl_delay_epoch) < 10000) {
-      // iIn the event where the buzzer time reference needs to be reset
+    while ( ( ( current_time = millis() ) - tl_delay_epoch) < TL_DELAY_RED_MS) {
+      // In the event where the buzzer time reference needs to be reset
       // Reset and toggle the buzzer
-      if ((current_time - buzzer_delay_epoch) < buzzer_countdown_ms) {
+      if ((current_time - buzzer_delay_epoch) >= buzzer_countdown_ms) {
         buzzer_delay_epoch = current_time;
         if (!buzzer_on) {
-          Serial.println("Buzzer on");
+          // Serial.println("Red: Buzzer on");
           tone(BUZZER_GPIO_NUM, BUZZER_FREQ);
           buzzer_on = true;
         } else {
-          Serial.println("Buzzer off");
+          // Serial.println("Red: Buzzer off");
           tone(BUZZER_GPIO_NUM, 0);
           buzzer_on = false;
         }
@@ -69,7 +67,7 @@ void loop() {
   case TL_STATE_RED_YELLOW: {
     // Simple State, just idle for 2 secs
     digitalWrite(TL_YELLOW_GPIO_NUM, HIGH);
-    delay(2000);
+    delay(TL_DELAY_YELLOW_MS);
     digitalWrite(TL_YELLOW_GPIO_NUM, LOW);
     tl_state = TL_STATE_GREEN;
     break;
@@ -77,7 +75,7 @@ void loop() {
   case TL_STATE_YELLOW: {
     // Simple State, just idle for 2 secs
     digitalWrite(TL_YELLOW_GPIO_NUM, HIGH);
-    delay(2000);
+    delay(TL_DELAY_YELLOW_MS);
     digitalWrite(TL_YELLOW_GPIO_NUM, LOW);
     tl_state = TL_STATE_RED;
     break;
@@ -85,40 +83,65 @@ void loop() {
   case TL_STATE_GREEN: {
     digitalWrite(TL_GREEN_GPIO_NUM, HIGH);
 
+    // The buzzer is off upon entering the green light (other 
+    // states manually set this)
+    // Upon entering the buzzer is off and the countdown begins
+    buzzer_countdown_ms = TL_DELAY_RED_BUZZER_OFF_MS;
+
     // Spin until button is high
+    current_time = millis();
+    buzzer_delay_epoch = current_time;
     while (!app_button_pushed()) {
-
-    }
-
-    Serial.println("button pushed");
-
-    // Begin the countdown timer for 5 seconds
-    buzzer_delay_epoch = millis();
-    uint16_t buzzer_green_state_countdown = 500;
-    while (digitalRead(BUTTON_GPIO_NUM) == 0) { 
       current_time = millis();
-      if ( ( current_time - buzzer_delay_epoch) < buzzer_green_state_countdown) {
+      if ( (current_time - buzzer_delay_epoch) >= buzzer_countdown_ms ) {
         buzzer_delay_epoch = current_time;
+        // Toggle buzzer (as well as the countdown)
         if (!buzzer_on) {
-          Serial.println("Buzzer on");
-          tone(BUZZER_GPIO_NUM, BUZZER_FREQ);
-          buzzer_on = true;
-          buzzer_green_state_countdown = 1500;
+            // Set the countdown to 500 (to remain on)
+            // Serial.println("Green: Buzzer on");
+            buzzer_countdown_ms = TL_DELAY_RED_BUZZER_ON_MS;
+            buzzer_on = true;
+            tone(BUZZER_GPIO_NUM, BUZZER_FREQ);
         } else {
-          Serial.println("Buzzer off");
-          tone(BUZZER_GPIO_NUM, 0);
-          buzzer_on = false;
-          buzzer_green_state_countdown = 5000;
+            // Set the countdown to 1500 (to remain off)
+            // Serial.println("Green: Buzzer off");
+            buzzer_countdown_ms = TL_DELAY_RED_BUZZER_OFF_MS;
+            buzzer_on = false;
+            tone(BUZZER_GPIO_NUM, 0);
         }
       }
     }
 
-    tl_delay_epoch = millis();
-    while ( ( millis() - tl_delay_epoch ) < 5000 ) {
-      // same logic as the spin loop
+    // Begin the countdown timer for 5 seconds
+    // Technically this could be combined in the previous while loop
+    // but this implementation is easier
+    current_time = millis();
+    tl_delay_epoch = current_time;
+    while ( ( ( current_time = millis() ) - tl_delay_epoch) < TL_DELAY_GREEN_MS) {
+      if ( (current_time - buzzer_delay_epoch) >= buzzer_countdown_ms ) {
+        buzzer_delay_epoch = current_time;
+        // Toggle buzzer (as well as the countdown)
+        if (!buzzer_on) {
+            // Set the countdown to 500 (to remain on)
+            // Serial.println("Green: Buzzer on");
+            buzzer_countdown_ms = TL_DELAY_RED_BUZZER_ON_MS;
+            buzzer_on = true;
+            tone(BUZZER_GPIO_NUM, BUZZER_FREQ);
+        } else {
+            // Set the countdown to 1500 (to remain off)
+            // Serial.println("Green: Buzzer off");
+            buzzer_countdown_ms = TL_DELAY_RED_BUZZER_OFF_MS;
+            buzzer_on = false;
+            tone(BUZZER_GPIO_NUM, 0);
+        }
+      }
     }
 
+    // Turn off everything and prepare to go to the next state
+    tone(BUZZER_GPIO_NUM, 0);
+    buzzer_on = false;
     digitalWrite(TL_GREEN_GPIO_NUM, LOW);
+    tl_state = TL_STATE_YELLOW;
     break;
   }
   default: {
@@ -136,6 +159,10 @@ void app_periph_init() {
 
   // Pedestrian button gpio configurations
   pinMode(BUTTON_GPIO_NUM, INPUT);
+
+  // Initialize LEDC for PWM for tone
+  ledcSetup(BUZZER_CHANNEL, BUZZER_FREQ, BUZZER_RESOLUTION);
+  ledcAttachPin(BUZZER_GPIO_NUM, BUZZER_CHANNEL);
 }
 
 bool app_button_pushed() {
